d4ddfb3ffed19deab79b01a653d3eef2b9af7e0f
Denis Khlopin <khlopin_den@mail.ru>
2021-03-29 20:25:31 +0500

    Initial Commit

---
+++ petnashki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  petnashki
+//
+//  Created by Denis Khlopin on 29/03/2021.
+//  Copyright © 2021 Denis Khlopin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
